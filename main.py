fae6f445210ab90c4e12159e9ce194fd8881223c
unknown <proudguy69>
2023-03-20 20:13:58 -0400

    huge changes to front end/dashboard

--- main.py
+++ main.py
@@ -10,8 +10,6 @@ SETTINGS = json.load(f)
 app = flask.Flask(__name__)
 DISCORD_ENDPOINT = 'https://discord.com/api/v10/'
 
-print(SETTINGS['CLIENTID'])
-print(SETTINGS['CLIENTSECRET'])
 
 @app.route('/')
 def index():
@@ -93,7 +91,6 @@ def serverpage():
         for server in data:
             perms = discord.Permissions(int(server['permissions']))
             if perms.manage_guild:
-                print(server)
                 if server['icon'] != None:
                     icon = f"https://cdn.discordapp.com/icons/{server['id']}/{server['icon']}.png"
                     serverobj = {'name': server['name'], 'icon':icon, 'id':server['id']}
@@ -103,13 +100,34 @@ def serverpage():
                 
                 servers.append(serverobj)
             
-        return flask.render_template('dashboard.html', user=flask.request.cookies.get('username'), avatar=flask.request.cookies.get('avatar'), servers=servers)
+        return flask.render_template('serverpage.html', user=flask.request.cookies.get('username'), avatar=flask.request.cookies.get('avatar'), servers=servers)
     else:
         return flask.redirect('/login')
     
-@app.route('/dashboard/<serverid>')
+@app.route('/dashboard/<serverid>', methods=['GET', 'POST'])
 def dashboard(serverid):
-    print(serverid)
-    return "hi"
+    if flask.request.method == 'GET':
+        token = flask.request.cookies.get('access_token')
+        if not token:
+            return flask.redirect('/login')
+        #this is where we would load server information
+        #check if the bot is in the server, if not, invite it
+        r = requests.get(f'{DISCORD_ENDPOINT}guilds/{serverid}', headers={'Authorization': f'Bot {SETTINGS["TOKEN"]}'})
+        if r.status_code == 404:
+            return flask.redirect('https://discord.com/api/oauth2/authorize?client_id=817049551843229736&permissions=8&redirect_uri=http%3A%2F%2F127.0.0.1%3A5000%2Fdashboard&response_type=code&scope=bot%20identify%20applications.commands')
+        if r.status_code == 200:
+            r = requests.get(f'{DISCORD_ENDPOINT}guilds/{serverid}/channels', headers={'Authorization': f'Bot {SETTINGS["TOKEN"]}'})
+            channelobjs = r.json()
+            return flask.render_template('dashboard.html', user=flask.request.cookies.get('username'), avatar=flask.request.cookies.get('avatar'),channels=channelobjs)
+    elif flask.request.method == 'POST':
+        #save the users data here
+        #I havent done this yet obviouisly
+        print(flask.request.json)
+        r = requests.get(f'{DISCORD_ENDPOINT}guilds/{serverid}/channels', headers={'Authorization': f'Bot {SETTINGS["TOKEN"]}'})
+        channelobjs = r.json()
+        return flask.render_template('dashboard.html', user=flask.request.cookies.get('username'), avatar=flask.request.cookies.get('avatar'),channels=channelobjs)
 
 app.run()
+
+
+
--- static/dashboard.js
+++ static/dashboard.js
@@ -1,8 +1,44 @@
-const servers = document.getElementsByClassName('server')
+document.querySelector("#dashboard-module-container").addEventListener("click", e => {
+    const enableBtn = e.target.closest("button.enable-button");
+    if(!enableBtn) return;
+    const container = enableBtn.closest(".dashboard-module");
+    container.classList.toggle('module-enabled')
+    container.firstElementChild.firstElementChild.firstElementChild.classList.toggle('switcher-enabled')
+  }, {passive: true});
 
 
-for (server in servers) {
-    servers[server].addEventListener('mouseenter', (event) => {
-        console.log(event)
-    })
+function welcomeSave() {
+  const switcher = document.querySelector('div.button-switcher')
+  let enabled = false
+  let dmMsg;
+  let channelMsg;
+  let channel;
+
+  if (switcher.classList.contains('switcher-enabled')) {
+    enabled = true
+  } else {
+    enabled = false
+  }
+
+  dmMsg = document.querySelector('#dmmsg').value
+  channel = document.querySelector('#welcchanneldropdown').value
+  channelMsg = document.querySelector('.channelinput').value
+  if (!dmMsg) {dmMsg = null}
+  if (!channel) {channel = null}
+  if (!channelMsg) {channelMsg = null}
+  const form = {
+    "enabled": enabled,
+    "dmMessage": dmMsg,
+    "channel": channel,
+    "channelMsg": channelMsg
+  }
+  const data = JSON.stringify(form)
+  fetch(window.location.href, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+  },
+    body: data
+  })
 }
