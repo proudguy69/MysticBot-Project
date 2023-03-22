9564dbcfe460c1d17fd084246e2be843559bdcd8
unknown <proudguy69>
2023-03-22 12:47:14 -0400

    changed up the module a bit

--- static/dashboard.js
+++ static/dashboard.js
@@ -7,38 +7,72 @@ document.querySelector("#dashboard-module-container").addEventListener("click", 
   }, {passive: true});
 
 
-function welcomeSave() {
-  const switcher = document.querySelector('div.button-switcher')
-  let enabled = false
-  let dmMsg;
-  let channelMsg;
-  let channel;
+//add a dm message for the welcome module
 
-  if (switcher.classList.contains('switcher-enabled')) {
-    enabled = true
-  } else {
-    enabled = false
+function removedm(e) {
+  const label = e.target.parentElement
+  const textarea = label.nextElementSibling
+  textarea.remove()
+  label.remove()
+  //rename all the other elements
+  const form = document.querySelector('#welcome-form')
+  let num = 0
+  for (i=0; i < form.children.length; i++) {
+    if (form.children[i].nodeName == 'LABEL') {
+      num++
+      form.children[i].innerText = `Dm Message #${num} | `
+      const span = document.createElement('span')
+      span.innerHTML = '- Remove'
+      span.id = "WelcomeDmRemove"
+      span.classList.add('module-button-red')
+      form.children[i].insertAdjacentElement('beforeend', span)
+      span.addEventListener('click', removedm)
+    }
+    if (form.children[i].nodeName == 'BR') {
+      break
+    }
   }
-
-  dmMsg = document.querySelector('#dmmsg').value
-  channel = document.querySelector('#welcchanneldropdown').value
-  channelMsg = document.querySelector('.channelinput').value
-  if (!dmMsg) {dmMsg = null}
-  if (!channel) {channel = null}
-  if (!channelMsg) {channelMsg = null}
-  const form = {
-    "enabled": enabled,
-    "dmMessage": dmMsg,
-    "channel": channel,
-    "channelMsg": channelMsg
-  }
-  const data = JSON.stringify(form)
-  fetch(window.location.href, {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json'
-  },
-    body: data
-  })
 }
+
+document.querySelector('#WelcomeDmRemove').addEventListener('click', removedm)
+
+
+function WelcomeDmAdd() {
+  //first, get the number of dm messages there is
+  const form = document.querySelector('#welcome-form')
+  let amt = 0
+  let last;
+  for (i=0; i < form.children.length; i++) {
+    if (form.children[i].nodeName == 'BR') {
+      amt = amt/2
+      last = form.children[i-1]
+      break
+    }
+    amt++
+  }
+  let num = amt+1
+  //create label
+  const label = document.createElement('label')
+  label.for = `dmmsg${num}`
+  label.innerHTML = `Dm Message #${num} | `
+  const span = document.createElement('span')
+  span.innerHTML = '- Remove'
+  span.id = "WelcomeDmRemove"
+  span.classList.add('module-button-red')
+  const textarea = document.createElement('textarea')
+  textarea.name = `dmmsg${num}`
+  textarea.rows = 2
+  textarea.classList.add('form-textarea')
+  if (!last) {
+    last = form
+    console.log("YES")
+    last.insertAdjacentElement('afterbegin', label)
+  } else {
+    last.insertAdjacentElement('afterend', label)
+  }
+  label.insertAdjacentElement('beforeend', span)
+  label.insertAdjacentElement('afterend', textarea)
+
+  span.addEventListener('click', removedm)
+}
+
