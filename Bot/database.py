526693fd90307dcc1e2dafa8f6da85223421e0ed
Paul Bouvier <idioticbatteringram@gmail.com>
2023-08-23 22:57:40 -0400

    Changes to db, working on setup welcome cmd

--- Bot/database.py
+++ Bot/database.py
@@ -16,9 +16,16 @@ password = data["DBKEY"]
 
 
 class Database():
+    # this will be ran when the bot joins a server, needs to create all tables for said server
     async def createServer(sid):
         conn = await asyncpg.connect(host="127.0.0.1", port=1101, user="postgres", password=password, database="main")
         await conn.execute("INSERT INTO servers(SID) VALUES($1)", sid)
+        await conn.execute("INSERT INTO welcome_conf(SID) VALUES($1)", sid)
+
     
 
+    # this is bad database design and class design, but this is currently here as example
+    # async def createWelcome_Conf(sid, content):
+    #    conn = await asyncpg.connect(host="127.0.0.1", port=1101, user="postgres", password=password, database="main")
+    #    await conn.execute("INSERT INTO welcome_conf(SID, content) VALUES($1, $2)", sid, content)
 
--- Bot/setup.py
+++ Bot/setup.py
@@ -1,8 +1,8 @@
 #basic module to set up database and what not
-
 import discord
 from discord.ext import commands
-from discord import app_commands
+from discord import app_commands, ui
+
 
 from database import Database
 
@@ -14,16 +14,24 @@ class setupGroup(app_commands.Group):
 
 
 
+class welcomeModal(ui.Modal, title="Welcome Config"):
+
+    content = ui.TextInput(label="Content", style=discord.TextStyle.paragraph, placeholder="The message that will be sent")
+
+    async def on_submit(self, interaction:discord.Interaction):
+        await interaction.response.send_message("thanks for submitting the welcome message!")
+        await Database.createWelcome_Conf(interaction.guild.id, content=self.content.value)
+
+
+
 class Setup(commands.Cog):
     def __init__(self) -> None:
         super().__init__()
+
     g = setupGroup()
 
+    # the plan for this is to have a select menu, inside are options like "content" (will open modal) and "add embed" (will open another modal) and it will live-edit the original message to show what the welcome message will become,, I need to configure the database for this lol
     @g.command()
     async def welcome(self, inte:discord.Interaction):
-        await inte.response.send_message("This is a test with example data!")
-        await Database.createServer(inte.guild.id)
-
-
-    
-    
+        await inte.response.send_modal(welcomeModal())
+        await Database.createServer(inte.guild.id)    
